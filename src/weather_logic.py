a6b980d3218f80b566be5fa8a1ef75d30b98a0ed
zxxfog <2488584108@qq.com>
2019-07-19 11:52:49 +0800

    提交初版程序代码

---
+++ src/weather_logic.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+本软件为自己工作之余学习python所做，主要是想学习PyQt5的使用及了解网络编程。
+本人水平较低，难免有不足与错误之处，还望各位大佬指正，也希望自己在此过程中能扩展自己的知识面，提升自己的编程水平
+"""
+
+# Note:本软件使用 和风天气 提供的天气预报API，需要注册以获取key， 地址 https://dev.heweather.com/
+# date:       2019/07/19
+# dev os/ver: windows7-64bit, python3.7
+# author:     gorkon
+# soft ver:   V0.0.1
+# change log: 创建程序
+
+
+from weather_ui import Ui_Class
+import sys
+import time
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication
+from PyQt5.QtCore import *
+import requests
+
+#需要获取天气的城市，拼音
+CITY_NAME = "beijing"
+MY_WEATHER_KEY = "80aa4504b70a4de4bf27bf4c521dc362"
+
+#获取天气信息线程
+class Update_Thread(QThread):
+
+    #自定义信号
+    wt_up_now_sig = pyqtSignal(str, str, str, int)  #分别为城市名、天气、时间、是否坏天气
+    wt_up_oth_sig = pyqtSignal(int, str, str, int)  #分别为明天(1)或后天(2)、天气、时间、是否坏天气
+    
+    def __init__(self):
+        super().__init__()
+        self.working = True
+        self.url_now="https://free-api.heweather.net/s6/weather/now?location="+CITY_NAME+"&key="+MY_WEATHER_KEY
+        self.url="https://free-api.heweather.net/s6/weather/forecast?location="+CITY_NAME+"&key="+MY_WEATHER_KEY
+        self.bad_wea=["小雨","中雨","大雨","阵雨","雷阵雨","暴雨","大暴雨","特大暴雨","小到中雨","中到大雨","大到暴雨","暴雨到大暴雨","雨"]
+    
+    #判断 获取的天气信息是否有雨
+    # todo 实现太笨，待优化
+    def __is_bad_weather(self, wae_str):
+        if wae_str in self.bad_wea:
+            return 1
+        else:
+            return 0
+            
+    #线程执行任务       
+    def run(self):
+        while self.working == True:
+            try:
+                #获取天气数据，和风天气API使用天气类型now
+                rs_we = requests.get(self.url_now).json()
+                #分别获取 城市名称、当前时间、天气信息 信息
+                city_name = rs_we["HeWeather6"][0]["basic"]["location"]
+                time_info = rs_we["HeWeather6"][0]["update"]["loc"]
+                wea_info_str = rs_we["HeWeather6"][0]["now"]["cond_txt"]
+                weat_info = (wea_info_str + "  " + rs_we["HeWeather6"][0]["now"]["tmp"] + "度  "+
+                            rs_we["HeWeather6"][0]["now"]["wind_dir"] + "  " + rs_we["HeWeather6"][0]["now"]["wind_sc"] + "级")
+                detag = self.__is_bad_weather(wea_info_str)
+                #如果是有雨天气
+                if detag == 1:
+                    #触发信号
+                    self.wt_up_now_sig.emit(city_name, weat_info, time_info, 1)
+                else:
+                    self.wt_up_now_sig.emit(city_name, weat_info, time_info, 0)
+                
+                #获取天气信息，和风天气使用关键字 forecast
+                rs_we = requests.get(self.url).json()
+                #获取返回数据中的 明天 的天气信息
+                time_info = rs_we["HeWeather6"][0]["daily_forecast"][1]["date"]
+                wea_info_str = rs_we["HeWeather6"][0]["daily_forecast"][1]["cond_txt_d"]
+                weat_info = (wea_info_str + "  " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][1]["tmp_min"] + " ~ " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][1]["tmp_max"] + "度  " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][1]["wind_dir"] + " " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][1]["wind_sc"] + "级")
+                detag = self.__is_bad_weather(wea_info_str)
+                if detag == 1:
+                    self.wt_up_oth_sig.emit(1, weat_info, time_info, 1)
+                else:
+                    self.wt_up_oth_sig.emit(1, weat_info, time_info, 0)
+                    
+                #获取返回数据中的 后天 的天气信息
+                time_info = rs_we["HeWeather6"][0]["daily_forecast"][2]["date"]
+                wea_info_str = rs_we["HeWeather6"][0]["daily_forecast"][2]["cond_txt_d"]
+                weat_info = (wea_info_str + "  " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][2]["tmp_min"] + " ~ " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][2]["tmp_max"] + "度  " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][2]["wind_dir"] + " " + 
+                            rs_we["HeWeather6"][0]["daily_forecast"][2]["wind_sc"] + "级")
+                detag = self.__is_bad_weather(wea_info_str)
+                if detag == 1:
+                    self.wt_up_oth_sig.emit(2, weat_info, time_info, 1)
+                else:
+                    self.wt_up_oth_sig.emit(2, weat_info, time_info, 0)
+                
+                time.sleep(300)
+            except Exception as e:
+                pass
+    
+#主类
+class Weather_Class(QObject):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Class()
+        self.up_thread = Update_Thread()
+    
+    #更新 当前天气 信息
+    def up_now_wea(self, str_city, str_wea, str_time, bad_tag):
+        self.ui.update_location(str_city)
+        if bad_tag==1:
+            self.ui.set_now_weathercolor("red")
+        else:
+            self.ui.set_now_weathercolor("blue")
+        self.ui.update_now_weather(str_wea)
+        self.ui.update_time(str_time)
+    
+    #更新 明天或后天 天气信息
+    def up_otr_wea(self, index, str_wea, str_time, bad_tag):
+        if index==1:    # 明天
+            self.ui.set_label(1, str_time)
+            if bad_tag==1:
+                self.ui.set_otr_weathercolor(1, "red")
+            else:
+                self.ui.set_otr_weathercolor(1, "blue")
+            self.ui.update_1_weather(str_wea)
+        else:
+            self.ui.set_label(2, str_time)
+            if bad_tag==1:
+                self.ui.set_otr_weathercolor(2, "red")
+            else:
+                self.ui.set_otr_weathercolor(2, "blue")
+            self.ui.update_2_weather(str_wea)
+            
+        
+    #将自定义信号和槽函数关联，并启动获取天气信息的线程
+    def update_wea_ui(self):
+        self.up_thread.wt_up_now_sig.connect(self.up_now_wea)
+        self.up_thread.wt_up_oth_sig.connect(self.up_otr_wea)
+        self.up_thread.start()
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wea = Weather_Class()
+    wea.update_wea_ui()
+    sys.exit(app.exec_())
+    
---
+++ src/weather_ui.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+本软件为自己工作之余学习python所做，主要是想学习PyQt5的使用及了解网络编程。
+本人水平较低，难免有不足与错误之处，还望各位大佬指正，也希望自己在此过程中能扩展自己的知识面，提升自己的编程水平
+"""
+
+# Note:本软件使用 和风天气 提供的天气预报API，需要注册以获取key， 地址 https://dev.heweather.com/
+# date:       2019/07/19
+# dev os/ver: windows7-64bit, python3.7
+# author:     gorkon
+# soft ver:   V0.0.1
+# change log: 创建程序
+
+
+import sys
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QLineEdit, QDesktopWidget, QGridLayout
+from PyQt5.QtGui import QFont
+
+#UI类
+class Ui_Class(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.city = QLabel("当前城市",self)
+        self.time = QLabel("当前时间",self)
+        self.weath= QLabel("当前天气",self)
+        self.weath_1= QLabel("明天天气",self)
+        self.weath_2= QLabel("后天天气",self)
+        self.city_line = QLineEdit()
+        self.time_line = QLineEdit()
+        self.weat_line = QLineEdit()
+        self.weat_line_1 = QLineEdit()
+        self.weat_line_2 = QLineEdit()
+        self.init_ui()
+        
+    def init_ui(self):
+        ui_font = QFont("宋体",12)
+        self.setFont(ui_font)
+        self.resize(480,320)
+        self.setWindowTitle("天气")
+        
+        #将窗体部署到屏幕中间
+        screen_size = QDesktopWidget().screenGeometry()
+        app_ui_size = self.geometry()
+        self.move((screen_size.width()-app_ui_size.width())/2, (screen_size.height()-app_ui_size.height())/2)
+        
+        #设置lineedit不可编辑
+        self.city_line.setDisabled(True)
+        self.time_line.setDisabled(True)
+        self.weat_line.setDisabled(True)
+        self.weat_line_1.setDisabled(True)
+        self.weat_line_2.setDisabled(True)
+        
+        #控件布局
+        grid = QGridLayout()
+        grid.setSpacing(2)
+        grid.addWidget(self.city, 1, 0)
+        grid.addWidget(self.city_line, 1, 1)
+        grid.addWidget(self.time, 2, 0)
+        grid.addWidget(self.time_line, 2, 1)
+        grid.addWidget(self.weath, 3, 0)
+        grid.addWidget(self.weat_line, 3, 1)
+        grid.addWidget(self.weath_1, 4, 0)
+        grid.addWidget(self.weat_line_1, 4, 1)
+        grid.addWidget(self.weath_2, 5, 0)
+        grid.addWidget(self.weat_line_2, 5, 1)
+        
+        self.setLayout(grid) 
+
+        self.show()
+    
+    #更新 当前天气 信息
+    def update_now_weather(self, str_info):
+        self.weat_line.setText(str_info)
+    
+    #更新当前时间信息
+    def update_time(self, str_info):
+        self.time_line.setText(str_info)
+    
+    #更新 明天天气 信息
+    def update_1_weather(self, str_info):
+        self.weat_line_1.setText(str_info)
+        
+    #更新 后天天气 信息
+    def update_2_weather(self, str_info):
+        self.weat_line_2.setText(str_info)
+    
+    #设置 当前天气 字体颜色
+    def set_now_weathercolor(self, str_color):
+        self.weat_line.setStyleSheet("color:"+str_color)
+        
+    #更新当前城市信息
+    def update_location(self, str_info):
+        self.city_line.setText(str_info)
+     
+    #设置 label 内容，index为1表示明日天气， 为2表示后天天气
+    def set_label(self, index, str):
+        if index == 1:
+            self.weath_1.setText(str)
+        elif index == 2:
+            self.weath_2.setText(str)
+        else:
+            pass
+     
+    #设置 明天或后天 天气字体颜色
+    def set_otr_weathercolor(self, index, str_color):
+        if index==1:
+            self.weat_line_1.setStyleSheet("color:"+str_color)
+        else:
+            self.weat_line_2.setStyleSheet("color:"+str_color)
+
+
